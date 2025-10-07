aa7e31581dbdfb511bfd644db7f5aaff34b085e1
Isabelle Nunes <isanunesalves07@gmail.com>
2025-10-07 19:46:28 -0300

    Adiciona paginas FAQ e Estrategias ao projeto

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-// adiciona .active no link que corresponde ao filename atual
+// Adiciona .active no link que corresponde ao filename atual
 (function(){
-  const current = location.pathname.split('/').pop() || 'index.html';
-  document.querySelectorAll('.nav a').forEach(a => {
-    const href = a.getAttribute('href');
-    if(href === current) a.classList.add('active');
-  });
+    const current = location.pathname.split('/').pop() || 'index.html';
+    document.querySelectorAll('.nav a').forEach(a => {
+        const href = a.getAttribute('href');
+        if(href === current) a.classList.add('active');
+    });
 })();
+
+// Código para a funcionalidade de clique do FAQ
+document.querySelectorAll('.faq-question').forEach(item => {
+    item.addEventListener('click', event => {
+        const parent = item.parentNode;
+        parent.classList.toggle('active');
+    });
+});
